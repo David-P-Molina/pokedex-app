133533ecdded8afd7431e4d219bf18c2821be456
David Moina <davemolina777@gmail.com>
2023-05-25 10:43:48 -0500

    Remove comments

--- src/apis/pokeAPI.js
+++ src/apis/pokeAPI.js
@@ -1,8 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-//What is createApi
-//What is fetchBase Query
 import { baseURL } from '../utils'
-//Why import baseURL and import it
 
 export const pokeApi = createApi({
     reducerPath: 'pokeApi',
@@ -10,4 +7,3 @@ export const pokeApi = createApi({
 })
 
 export const { useGetPokemonByNameQuery } = pokeApi
-//Why did ChatGPT write it like this?
