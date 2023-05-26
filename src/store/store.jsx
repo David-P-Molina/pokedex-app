387e72b8e89b59d94b1d0268b6114d1387f90782
David Moina <davemolina777@gmail.com>
2023-05-26 12:53:26 -0500

    Added comments for clarity

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -4,10 +4,10 @@ import pokemonReducer from "../reducers/pokemonSlice";
 
 const store = configureStore({
     reducer: {
-        [pokeApi.reducerPath]: pokeApi.reducer,
-        pokemon: pokemonReducer,
+        [pokeApi.reducerPath]: pokeApi.reducer,//specific to api calls and adding them to state
+        pokemon: pokemonReducer,//specific to any reducers needed
     },
-    middleware: (getDefaultMiddleware) => {
+    middleware: (getDefaultMiddleware) => {//part of redux toolkit adding middleware(thunk) to reducer
         getDefaultMiddleware().concat(pokeApi.middleware)
     }
 })
