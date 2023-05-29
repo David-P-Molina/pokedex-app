1abc0dcd88fe8fc0fee85f48373472b5a99ccde6
David Moina <davemolina777@gmail.com>
2023-05-28 22:15:51 -0500

    Import Route/s Box NavBar & home components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+
 import './App.css';
 
 function App() {
