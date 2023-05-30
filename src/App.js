046f141f405b2b2aa2286c623ea8fe92aa95b05d
David Moina <davemolina777@gmail.com>
2023-05-30 13:44:02 -0500

    Include BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Box } from '@mui/material'
 import NavBar from './components/NavBar';
 import Footer from './components/Footer'
@@ -13,11 +13,13 @@ function App() {
   return (
     <Box className="App">
       <NavBar />
-      <Routes>
-        <Route to="/" element={ <Home/> }/>  
-        <Route to="/pokemon" element={<PokemonDetail />}/>
-        <Route to="/types" element={<TypesDetail />}/>
-      </Routes>
+      <Router>
+        <Routes>
+          <Route to="/" element={ <Home/> }/>  
+          <Route to="/pokemon" element={<PokemonDetail />}/>
+          <Route to="/types" element={<TypesDetail />}/>
+        </Routes>
+      </Router>
       <Footer />
     </Box>
   );
