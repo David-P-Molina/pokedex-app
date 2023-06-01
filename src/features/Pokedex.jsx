c50268dc16bda95ff1cc899aa3689166d99f87d9
David Moina <davemolina777@gmail.com>
2023-06-01 11:42:30 -0500

    Create useSelector & dispatch and handleSearch functionality

--- src/features/Pokedex.jsx
+++ src/features/Pokedex.jsx
@@ -4,9 +4,12 @@ import { useSelector, useDispatch } from 'react-redux'
 import { fetchPokemon } from '../reducers/pokemonSlice'
 
 const Pokedex = () => {
-    const pokemon = useSelector((state) => state.pokemon.pokemon)
-    const dispatch = useDispatch()
+    const pokemon = useSelector((state) => state.pokemon.pokemon)  
+    const dispatch = useDispatch() 
 
+    const handleSearch = (name) => {
+        dispatch(fetchPokemon(name))
+    }
   return (
     <div>Pokedex</div>
   )
