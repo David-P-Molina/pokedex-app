30ed4db4f44ffa029cad78da882a37ce97fc5d42
David Moina <davemolina777@gmail.com>
2023-06-01 12:01:09 -0500

    Create endpoint for get by name

--- src/apis/pokeAPI.js
+++ src/apis/pokeAPI.js
@@ -4,6 +4,11 @@ import { baseURL } from '../utils'
 export const pokeApi = createApi({
     reducerPath: 'pokeApi',
     baseQuery: fetchBaseQuery({ baseUrl: baseURL}),
+    endpoints: (builder) => ({
+        getPokemonByName: builder.query({
+            query: (name)=> `pokemon/${name}`
+        })
+    }),
 })
 
 export const { useGetPokemonByNameQuery } = pokeApi
