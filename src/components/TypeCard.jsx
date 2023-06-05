5eca16c4f4338ee87194c2237093981bd53b1dea
David Moina <davemolina777@gmail.com>
2023-06-05 11:01:55 -0500

    Remove typeInfo logic and assemble typeInfo in initial fetchCall

--- src/components/TypeCard.jsx
+++ src/components/TypeCard.jsx
@@ -4,22 +4,21 @@ import { useSelector } from 'react-redux'
 
 const TypeCard = ({ type }) => {
     const typeInfo = useSelector((state) => state.types.typesList.name === type)
-    //Conditional if type === typeInfoName create damage info for that name
+    
   return (
     <Box>
         <Typography variant='h3'>
             {type.name}
         </Typography>
         <Typography>Stats:</Typography>
-        <Typography>Double Damage From: {typeInfo?.double_damage_from}</Typography>
-        <Typography>Double Damage To: {typeInfo?.double_damage_to}</Typography>
-        <Typography>Half Damage From: {typeInfo?.half_damage_from}</Typography>
-        <Typography>Half Damage To: {typeInfo?.half_damage_to}</Typography>
-        <Typography>No Damage From: {typeInfo?.no_damage_from}</Typography>
-        <Typography>No Damage To: {typeInfo?.no_damage_to}</Typography>
+        <Typography>Double Damage From: {type?.double_damage_from || 'None'}</Typography>
+        <Typography>Double Damage To: {type?.double_damage_to || 'None'}</Typography>
+        <Typography>Half Damage From: {type?.half_damage_from || 'None'}</Typography>
+        <Typography>Half Damage To: {type?.half_damage_to || 'None'}</Typography>
+        <Typography>No Damage From: {type?.no_damage_from || 'None'}</Typography>
+        <Typography>No Damage To: {type?.no_damage_to || 'None'}</Typography>
 
-    </Box>
-  )
+    </Box
 }
 
 export default TypeCard
