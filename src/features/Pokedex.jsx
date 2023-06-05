51608d5de6e841a70fb0fa71025244fbd1c7fecc
David Moina <davemolina777@gmail.com>
2023-06-04 21:31:34 -0500

    assemble pokemonList

--- src/features/Pokedex.jsx
+++ src/features/Pokedex.jsx
@@ -3,6 +3,7 @@ import { Grid, Typography, Box } from '@mui/material'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchPokemon, fetchAllPokemon, fetchAllTypes, fetchPokemonListAsync, fetchPokeInfoAsync } from '../reducers/pokemonSlice'
 import { pokeApi } from '../apis/pokeApi';
+import { constructPokeObject } from '../utils';
 
 const Pokedex = () => {
   const dispatch = useDispatch() 
@@ -25,15 +26,8 @@ const Pokedex = () => {
       const allPokemonInfo = []
       pokemonList.forEach((pokemon,index) => {
         let pokeInfo = dispatch(fetchPokeInfoAsync(pokemon))
-        // let pokeObject = {
-        //   height: pokeInfo['height'],
-        //   weight: pokeInfo['weight'],
-        //   types: [pokeInfo.types[0], pokeInfo.types[1]],
-        //   id: pokeInfo['id'],
-        //   // img: pokeInfo.sprites.versions.generation-viii.icons.front_default,
-        //   key: index,
-        // }; //construct necessary info, type, evolutions, description, etc.
-        // allPokemonInfo.push(pokeObject)
+        let assembledPokeObj = constructPokeObject(pokeInfo, index)
+        allPokemonInfo.push(assembledPokeObj)
       })
     }, [])
     
