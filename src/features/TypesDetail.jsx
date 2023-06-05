792591d81715e184f7e07fdf9f8a0da3fd6949f4
David Moina <davemolina777@gmail.com>
2023-06-04 22:22:55 -0500

    Import react/redux hooks and fetch and create dispatch/useSelectors

--- src/features/TypesDetail.jsx
+++ src/features/TypesDetail.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTypes } from './typesSlice';
 
 const TypesDetail = () => {
+  const dispatch = useDispatch();
+  const typesList = useSelector((state) => state.types.typesList);
+  const status = useSelector((state) => state.types.status);
+  const error = useSelector((state) => state.types.error);
   return (
     <div>TypesDetail</div>
   )
