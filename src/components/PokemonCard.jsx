c23e274c9a30df963e06796c3b724ed8897ade89
David Moina <davemolina777@gmail.com>
2023-06-04 21:35:41 -0500

    Create basic pokemonCard from pokedex.js

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material'
 
-const PokemonCard = () => {
+const PokemonCard = ({ pokemon }) => {
   return (
-    <div>PokemonCard</div>
+    <Box>
+            <Typography 
+              key={pokemon.name}
+              variant='h3' 
+              textTransform='capitalize'>
+              {pokemon.name}
+            </Typography>
+          </Box>
   )
 }
 
