0f48cab85cf79c046cb555ecfa2505b6ca412ac8
David Moina <davemolina777@gmail.com>
2023-06-04 22:28:14 -0500

    Create error state selector and pages if types is not readily available

--- src/features/TypesDetail.jsx
+++ src/features/TypesDetail.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTypes } from './typesSlice';
-
+import { Typography } from '@mui/material';
 const TypesDetail = () => {
   const dispatch = useDispatch();
-  const typesList = useSelector((state) => state.types.typesList);
-  const status = useSelector((state) => state.types.status);
+  const typesList = useSelector((state) => state.types.typesList)
+  const status = useSelector((state) => state.types.status)
+  const error = useSelector((state) => state.types.error)
   
   useEffect(() => {
     dispatch(fetchTypes())
   }, [dispatch])
   
+  if(status === 'loading') return <Typography variant='h4'>Loading...</Typography>
+
+  if(status === 'failed') return <Typography variant='h4'>Error: {error}</Typography>
   return (
     <div>TypesDetail</div>
   )
