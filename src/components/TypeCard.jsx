35c6cc50d1b82bc72f9afdecffb61e348dd19276
David Moina <davemolina777@gmail.com>
2023-06-05 10:53:09 -0500

    Create typography components listing damage to etc.

--- src/components/TypeCard.jsx
+++ src/components/TypeCard.jsx
@@ -6,14 +6,21 @@ const TypeCard = ({type}) => {
     //   fetchTypeDetail
     //Add to state and use
     //should this be done in the card or in the app types component
-    
     }, [])
-    
+    let typeInfo = {}
   return (
     <Box>
         <Typography variant='h3'>
             {type.name}
         </Typography>
+        <Typography>Stats:</Typography>
+        <Typography>Double Damage From: {typeInfo?.double_damage_from}</Typography>
+        <Typography>Double Damage To: {typeInfo?.double_damage_to}</Typography>
+        <Typography>Half Damage From: {typeInfo?.half_damage_from}</Typography>
+        <Typography>Half Damage To: {typeInfo?.half_damage_to}</Typography>
+        <Typography>No Damage From: {typeInfo?.no_damage_from}</Typography>
+        <Typography>No Damage To: {typeInfo?.no_damage_to}</Typography>
+
     </Box>
   )
 }
