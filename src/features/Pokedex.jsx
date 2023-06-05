abb44969ae4b2e36e65fd0f7161a863e547c1d2b
David Moina <davemolina777@gmail.com>
2023-06-04 21:29:04 -0500

    Move pokeObjectCreation to util.js

--- src/features/Pokedex.jsx
+++ src/features/Pokedex.jsx
@@ -25,17 +25,17 @@ const Pokedex = () => {
       const allPokemonInfo = []
       pokemonList.forEach((pokemon,index) => {
         let pokeInfo = dispatch(fetchPokeInfoAsync(pokemon))
-        let pokeObject = {
-          height: pokeInfo['height'],
-          weight: pokeInfo['weight'],
-          types: [pokeInfo.types[0], pokeInfo.types[1]],
-          id: pokeInfo['id'],
-          img: pokeInfo.sprites.versions.generation-viii.icons.front_default,
-          key: index,
-        }; //construct necessary info, type, evolutions, description, etc.
-        allPokemonInfo.push(pokeObject)
+        // let pokeObject = {
+        //   height: pokeInfo['height'],
+        //   weight: pokeInfo['weight'],
+        //   types: [pokeInfo.types[0], pokeInfo.types[1]],
+        //   id: pokeInfo['id'],
+        //   // img: pokeInfo.sprites.versions.generation-viii.icons.front_default,
+        //   key: index,
+        // }; //construct necessary info, type, evolutions, description, etc.
+        // allPokemonInfo.push(pokeObject)
       })
-    }, [pokemonList])
+    }, [])
     
     // useEffect(() => {
     //   // Log the fetched data
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,12 @@
 export const baseURL = 'https://pokeapi.co/api/v2/'
+
+export const constructPokeObject = (singlePokemon, index) => {
+    let pokeObject = {
+        height: pokeInfo['height'],
+        weight: pokeInfo['weight'],
+        types: [pokeInfo.types[0], pokeInfo.types[1]],
+        id: pokeInfo['id'],
+        // img: pokeInfo.sprites.versions.generation-viii.icons.front_default,
+        key: index,
+    }; //construct necessary info, type, evolutions, description, etc.
+}
