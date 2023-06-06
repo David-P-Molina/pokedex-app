94357c0f188703a0a0e5e3fb2e5953814d63a17a
David Moina <davemolina777@gmail.com>
2023-06-06 14:23:57 -0500

    Move pokemonList logic to pokemonDeatil.jsx,

--- src/features/PokemonDetail.jsx
+++ src/features/PokemonDetail.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Grid } from '@mui/material'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchPokemonListAsync } from '../reducers/pokemonSlice'
+import PokemonCard from '../components/PokemonCard';
 
 const PokemonDetail = () => {
+  const dispatch = useDispatch() 
+  const pokemonList = useSelector((state) => state.pokemon.pokemonList)
+  useEffect(() => {
+    dispatch(fetchPokemonListAsync(2))
+  }, [dispatch])
+
+  if(!pokemonList > 0) return "Loading..."
   return (
-    <div>PokemonDetail</div>
+    <Grid>
+        <h1>Pokemon List</h1>
+        {pokemonList.map((pokemon) => (
+          <PokemonCard key={pokemon.name} pokemon={pokemon} />
+          )
+        )}
+    </Grid>
   )
 }
 
