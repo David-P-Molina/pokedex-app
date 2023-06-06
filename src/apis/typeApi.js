9f62b3408b915c0d7b27f43363e9ad4d17c24d2c
David Moina <davemolina777@gmail.com>
2023-06-06 15:10:50 -0500

    Remove createApi logic and use fetch consts to avoid breaking

--- src/apis/typeApi.js
+++ src/apis/typeApi.js
@@ -1,17 +1,13 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { baseURL } from '../utils';
 
-export const typeApi = createApi({
-  reducerPath: 'typeApi',
-  baseQuery: fetchBaseQuery({ baseUrl: baseURL }),
-  endpoints: (builder) => ({
-    getAllTypes: builder.query({
-      query: () => 'type',
-    }),
-    getOneType: builder.query({
-      query: ({ typeURL }) => typeURL,
-    }),
-  }),
-});
+export const fetchAllTypes = async () => {
+    const response = await fetch(`${baseURL}type`)
+    const data = await response.json()
+    return data.results
+}
 
-export const { useGetAllTypesQuery, useGetOneTypeQuery } = typeApi;
+export const fetchOneType = async ({ typeURL }) => {
+    const response = await fetch(`${typeURL}`)
+    const data = await response.json()
+    return data
+}
