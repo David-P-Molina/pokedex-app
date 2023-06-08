6851d89b230b630e3db9883d3c8928c06b25aa2f
David Moina <davemolina777@gmail.com>
2023-06-08 13:49:37 -0500

    Create initial state

--- src/reducers/pokedexSlice.jsx
+++ src/reducers/pokedexSlice.jsx
@@ -1,3 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { fetchAllTypes, fetchOneType } from '../apis/typeApi'
 
+const initialState = {
+    pokedexList: [],
+    status: 'idle',
+    error: null,
+}
