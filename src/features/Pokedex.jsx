de3ff1a5cbd53046f7724a0d0724e98c2442bca7
David Moina <davemolina777@gmail.com>
2023-06-13 12:40:52 -0500

    Fix import for PokedexCard

--- src/features/Pokedex.jsx
+++ src/features/Pokedex.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
 import { Grid, Typography, Box } from '@mui/material'
 import { useSelector, useDispatch } from 'react-redux'
+import PokedexCard from '../components/PokedexCard'
 
 const Pokedex = () => {
   const dispatch = useDispatch() 
-  
+    
     useEffect(() => {
     }, [dispatch])
     
