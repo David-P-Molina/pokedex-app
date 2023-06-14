6a3ae581309f857d42bee08256040b4cda73e924
David Moina <davemolina777@gmail.com>
2023-06-14 12:16:44 -0500

    Deconstruct pokedex object and create error handling

--- src/components/PokedexCard.jsx
+++ src/components/PokedexCard.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const PokedexCard = () => {
+const PokedexCard = ({pokedex}) => {
+  try {
+    const {
+    version_groups: [{ name: versionGroupName = "", url: versionGroupUrl = "" } = {}] = [],
+    region: { name: regionName = "" } = {},
+    id = "",
+    name = "",
+    descriptions: [{ description = "" } = {}] = []
+  } = pokedex
+} catch (error) {
+  //national pokedex & conquest gallery pokedex are not working
+} finally {
+  
+}
   return (
-    <div className='pokedex-card'>PokedexCard</div>
+    <div className='pokedex-card'></div>
   )
 }
 
