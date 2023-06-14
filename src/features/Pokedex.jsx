42a16453a2db8628fab50ec79f16b85bac82b2ec
David Moina <davemolina777@gmail.com>
2023-06-14 11:31:09 -0500

    Create useSelector and import fetchCall

--- src/features/Pokedex.jsx
+++ src/features/Pokedex.jsx
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react'
 import { Grid, Typography, Box } from '@mui/material'
 import { useSelector, useDispatch } from 'react-redux'
 import PokedexCard from '../components/PokedexCard'
+import { fetchPokedexListAsync } from '../reducers/pokedexSlice'
 
 const Pokedex = () => {
   const dispatch = useDispatch() 
-    
+  const pokedexList = useSelector((state) => state.pokedex.pokedexList)
     useEffect(() => {
+      dispatch(fetchPokedexListAsync())
     }, [dispatch])
     
   return (
