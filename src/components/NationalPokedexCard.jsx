9c6165c32f7d37ef4c6b60c8f792e29d2c5e4d5f
David Moina <davemolina777@gmail.com>
2023-06-17 21:39:26 -0500

    Create englishDescription const

--- src/components/NationalPokedexCard.jsx
+++ src/components/NationalPokedexCard.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { Box, Typography,  } from '@mui/material'
 
 const NationalPokedexCard = (pokedex) => {
+    console.log(pokedex)
+    const { description: englishDescription } = pokedex.descriptions.find(
+        (desc) => desc.language.name === "en"
+      ) || {};
   return (
     <Box className='pokedex-card'>
       <Typography>ID: {id}</Typography>
