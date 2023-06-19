a77b612328b9980727961c8a4b6a0aacb43c9740
David Moina <davemolina777@gmail.com>
2023-06-18 20:03:40 -0500

    Deconstruct items for national pokedex

--- src/components/NationalPokedexCard.jsx
+++ src/components/NationalPokedexCard.jsx
@@ -3,17 +3,19 @@ import { Box, Typography,  } from '@mui/material'
 
 const NationalPokedexCard = (pokedex) => {
     console.log(pokedex)
-    const { description: englishDescription } = pokedex.descriptions.find(
-        (desc) => desc.language.name === "en"
-      ) || {};
+    const { id, pokedexName, regionName = "", versionGroupName = "" } = pokedex
+    // const { description: englishDescription } = pokedex.descriptions.find(
+    //     (desc) => desc.language.name === "en"
+    //   ) || {};
+    console.log(id)
+    console.log(pokedexName)
   return (
     <Box className='pokedex-card'>
       <Typography>ID: {id}</Typography>
       <Typography>PokedexName: {pokedexName}</Typography>
-      <Typography>Description: {englishDescription}</Typography>
+      {/* <Typography>Description: {englishDescription}</Typography> */}
       <Typography>Region Name: {regionName}</Typography>
       <Typography>Version Group Name {versionGroupName}</Typography>
-      <Typography>URL: {versionGroupUrl}</Typography>
     </Box>
   )
 }
